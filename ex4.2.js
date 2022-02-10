34ce3735915a4c8ab1542b87d31cb8b39903c576
PikhulyaYana <pikhulyayana@gmail.com>
2022-02-10 03:34:42 +0200

    Added 5 js files as a homework 09.02

---
+++ ex4.2.js
@@ -0,0 +1,17 @@
+/*Write a program which take number of month from user and 
+print out name of season (summer, winter ...)*/
+
+
+let month = prompt('Enter the number of month');
+if (res = (month ==12) || (month >=1 && month <=2)) {
+    console.log ("Winter");
+}
+if (res = month >=3 && month <=5) {
+    console.log ("Spring");
+}
+if (res = month >=6 && month <=8) {
+    console.log ("Summer");
+}
+if (res = month >=9 && month <=11) {
+    console.log ("Fall");
+}
